ce42551ac4093b3ebd1374f9dcdbda8d6e29ed08
Hex-Bit <hasaanmahmudul@gmail.com>
2018-03-17 02:00:30 +0600

    Day2 Updating

---
+++ Bmw.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day2;
+
+/**
+ *
+ * @author 5H1MU7
+ */
+public class Bmw {
+    public static void main(String[] args) {
+        Bmw_xseries series=new Bmw_xseries();
+        
+        Bmw_xseries series2=new Bmw_xseries();
+        series.seta(234);
+        System.out.println(series.geta());
+       
+        
+        
+    }
+}
---
+++ Bmw_xseries.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Day2;
+
+public class Bmw_xseries {
+
+    
+    private int a;
+    public void seta(int a)
+    {
+        this.a=a;
+    }
+    public int geta()
+    {
+        return a;
+    }
+
+    
+    
+}
